37b22305b3fa6981329d9312daa499916a0ed37a
Cao quy Le <mikamokedo@gmail.com>
2023-07-27 11:11:55 +0700

    fix: cors 3

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const port = 3005;
 var bodyParser = require('body-parser')
 var cors = require('cors')
 app.use(bodyParser.json())
-const allowedOrigins = ["https://h5.zdn.vn/", "zbrowser://h5.zdn.vn/"];
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  // if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  // }
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  return next();
-});
-
-
 app.get('/', function (req, res) {
   res.send('Hello World! version 1.2');
 });
+// const allowedOrigins = ["https://h5.zdn.vn/", "zbrowser://h5.zdn.vn/"];
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
+//   // if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   // }
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   return next();
+// });
+
+
+
 
 app.post('/push-info', function (req, res) {
   const body = req.body;
